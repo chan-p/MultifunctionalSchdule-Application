f22d07c120b0812a5575bbecb3cf6f558a2affa8
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2016-11-09 02:04:36 +0900

    add

--- application.go
+++ application.go
@@ -36,6 +36,7 @@ func main(){
   e.Get("/email",tool.Res_mysql())
   e.Get("/calender",model.Echo_event(db))
   e.Get("/regist",model.Echo_regist(db))
+  e.Get("/cmd",tool.Res_cmd())
 
   //サーバー構築 ポート1323
   e.Run(standard.New(":1323"))
--- tool/exec.go
+++ tool/exec.go
@@ -4,15 +4,19 @@ import (
     "net/http"
     "os/exec"
     "time"
+    "fmt"
     "github.com/labstack/echo"
 )
 
-func cmd() echo.HandlerFunc {
+func Res_cmd() echo.HandlerFunc {
     return func(c echo.Context) error {
-        exec.Command("sleep", "5s").Start()
+        cmd := exec.Command("sleep", "5s")
         fmt.Println("sleep中: ", time.Now().Format("15:04:05"))
-        cmd.Wait()
         fmt.Println("sleep終了: ", time.Now().Format("15:04:05"))
+        exec.Command("GOOS=linux GOARCH=amd64 go build ../application.go").Start()
+        cmd.Start()
+        exec.Command(".././application").Start()
+        cmd.Wait()
         return c.String(http.StatusOK, "Hello World")
     }
 }
