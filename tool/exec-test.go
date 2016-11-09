602abc49b1947222053256c9f4ba112a3fb463f1
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2016-11-09 14:04:13 +0900

    update

---
+++ tool/exec-test.go
@@ -0,0 +1,22 @@
+package tool
+
+import (
+    "net/http"
+    _"os/exec"
+    _ "time"
+    _ "fmt"
+    "github.com/labstack/echo"
+    "github.com/codeskyblue/go-sh"
+)
+
+func Test_cmd() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        sh.Command("git", "pull").Run()
+        //cmd := exec.Command("git","log")
+        //cmd.Start()
+        //cmd.Wait()
+        //a, _ := exec.Command("sh", "-c", "git log --oneline | wc -l").Output()
+        //fmt.Println(string(out))
+        return c.String(http.StatusOK, "Heoollo World")
+    }
+}
