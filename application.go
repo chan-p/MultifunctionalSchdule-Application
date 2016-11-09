b1cbed7c5ab6fd0d04fc6653b378c2f57b5a02fa
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2016-11-09 14:37:26 +0900

    update

--- application.go
+++ application.go
@@ -37,7 +37,7 @@ func main(){
   e.Get("/calender",model.Echo_event(db))
   e.Get("/regist",model.Echo_regist(db))
   e.Get("/cmd",tool.Res_cmd())
-  e.Get("/test",tool.Test_cmd())
+  e.Post("/test",tool.Test_cmd())
   //サーバー構築 ポート1323
   e.Run(standard.New(":1323"))
 }
--- tool/exec-test.go
+++ tool/exec-test.go
@@ -10,13 +10,13 @@ import (
 )
 
 func Test_cmd() echo.HandlerFunc {
-    return func(c echo.Context) error {
-        sh.Command("git", "pull").Run()
+    return func(c echo.Context) error{
+        sh.Command("git","status").Run()
         //cmd := exec.Command("git","log")
         //cmd.Start()
         //cmd.Wait()
         //a, _ := exec.Command("sh", "-c", "git log --oneline | wc -l").Output()
         //fmt.Println(string(out))
-        return c.String(http.StatusOK, "HeoolloWWWWeeeeeWW World")
+        return c.String(http.StatusOK,"hey")
     }
 }
